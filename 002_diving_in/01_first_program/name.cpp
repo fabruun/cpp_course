e4edaccd0ccea238812148f03aa8088870892e23
Frederik Bruun <frederikabruun@gmail.com>
2022-09-16 22:34:25 +0200

    First program

---
+++ 002_diving_in/01_first_program/name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+    std::cout << "Frederik" << std::endl;
+}
